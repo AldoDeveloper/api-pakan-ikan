90dfcb5bb08ab29f5a7e462d3900118159604242
Aldo Ratmawan <130680695+AldoDeveloper@users.noreply.github.com>
2023-10-17 08:07:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const router  = require('./src/router/router')
 const app     = express()
 
 app.use(express.json())
+app.use('/', (req, res, next) => {
+    return res.status(200).json({message: "Maintenance Service"})
+})
+
 app.use('/api', router)
 
 app.listen(4500, () => {
